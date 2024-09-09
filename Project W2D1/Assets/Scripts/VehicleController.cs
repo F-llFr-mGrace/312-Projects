6ce6d2d663ed1f7a6c5945e3692568be9c67cba5
Grace Fowler <f.llfr.mgrace@gmail.com>
2024-09-09 10:21:16 -0400

    car vroom

---
+++ Project W2D1/Assets/Scripts/VehicleController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    [SerializeField] Rigidbody car;
+
+    [SerializeField] float speedMove = 1;
+    [SerializeField] float speedTurn = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical") * speedMove * Time.deltaTime;
+        float turn = Input.GetAxis("Horizontal") * speedTurn * Time.deltaTime;
+        Debug.Log($"Move: {move}, Turn: {turn}");
+        car.AddForce(this.transform.right * move * Time.deltaTime);
+        car.AddTorque(this.transform.up * turn * Time.deltaTime);
+        //car.AddForce();
+    }
+}
