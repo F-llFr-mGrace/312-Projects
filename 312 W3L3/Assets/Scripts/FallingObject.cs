8dddfb1cc2790c8b5d69378893c4260dbf8b323a
Grace Fowler <f.llfr.mgrace@gmail.com>
2024-09-15 02:52:55 -0400

    Added score multiplier based on player performance

---
+++ 312 W3L3/Assets/Scripts/FallingObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour
+{
+    [SerializeField] Rigidbody thisRigidbody;
+    [SerializeField] float waitTime;
+
+    bool timerActivated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > waitTime && !timerActivated)
+        {
+            timerActivated = true;
+            thisRigidbody.useGravity = true;
+        }
+    }
+}
--- 312 W3L3/Assets/Scripts/scorekeeping.cs
+++ 312 W3L3/Assets/Scripts/scorekeeping.cs
@@ -8,16 +8,39 @@ public class scorekeeping : MonoBehaviour
     [SerializeField] TextMeshProUGUI textScore;
 
     float score = 0;
-    float scoreToAdd;
+    float scoreMult = 1;
+    float lastScored;
+    float lastScoredOld;
 
     void Start()
     {
+        lastScored = Time.time;
+        lastScoredOld = Time.time;
         textScore.text = "Score: " + score;
     }
 
     public void addToScore(float scoreToAdd)
     {
+        if (lastScored < float.Epsilon)
+        {
+            lastScored = Time.time;
+            lastScoredOld = Time.time;
+        }
+        else
+        {
+            lastScored = Time.time;
+            if (lastScored - lastScoredOld <= 5f)
+            {
+                scoreMult += 0.1f;
+            }
+            else
+            {
+                scoreMult = 1;
+            }
+            lastScoredOld = lastScored;
+        }
         score += scoreToAdd;
-        textScore.text = "Score: " + score;
+        textScore.text = ("Score: " + score + " X " + scoreMult + " = " + score * scoreMult).ToString();
+        score *= scoreMult;
     }
 }
