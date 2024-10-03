eb668b155a088fcd39773e013208fd3a026cad4a
Grace Fowler <f.llfr.mgrace@gmail.com>
2024-10-02 20:57:21 -0400

    Added re-direct mechanic

---
+++ 312 W6L6/Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    [SerializeField] Rigidbody thisRigidbody;
+    [SerializeField] float speedThrust;
+    [SerializeField] float speedRot;
+
+    Vector3 thrust;
+    float rotateForce;
+
+    float momentum;
+    bool isStopped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateForce = Input.GetAxis("Horizontal");
+        thisRigidbody.AddTorque(-Vector3.forward * rotateForce * speedRot * 100 * Time.deltaTime);
+
+        thrust = transform.up * Input.GetAxis("Vertical");
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            thrust *= 0;
+        }
+
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            if (isStopped)
+            {
+                thisRigidbody.useGravity = true;
+                thisRigidbody.velocity = transform.up * momentum;
+            }
+            thisRigidbody.AddForce(thrust * speedThrust * 100 * Time.deltaTime);
+            isStopped = false;
+        }
+        else
+        {
+            if (!isStopped)
+            {
+                thisRigidbody.useGravity = false;
+                momentum = thisRigidbody.velocity.magnitude;
+                isStopped = true;
+            }
+            thisRigidbody.velocity = Vector3.zero;
+        }
+    }
+}
