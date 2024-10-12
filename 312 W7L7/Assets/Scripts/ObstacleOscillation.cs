29dda7ad9177fe123f551116a912cf6dceac1130
Grace Fowler <f.llfr.mgrace@gmail.com>
2024-10-11 21:21:04 -0400

    Obstacle movement

---
+++ 312 W7L7/Assets/Scripts/ObstacleOscillation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleOscillation : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period;
+
+    const float tau = Mathf.PI * 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSinWave + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
