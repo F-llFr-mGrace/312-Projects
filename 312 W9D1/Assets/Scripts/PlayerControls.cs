30260f5dab277d13bcbfb2628ee5a80514dc95fd
Grace Fowler <f.llfr.mgrace@gmail.com>
2024-10-25 19:45:53 -0400

    Start moving player + move player using input

--- 312 W9D1/Assets/Scripts/PlayerControls.cs
+++ 312 W9D1/Assets/Scripts/PlayerControls.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
+    [SerializeField] float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +27,21 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+        float horizontalThrow = movement.ReadValue<Vector2>().x * speed * 10 * Time.deltaTime;
+        float verticalThrow = movement.ReadValue<Vector2>().y * speed * 10 * Time.deltaTime;
 
         /*
         float horizontalThrow = Input.GetAxis("Horizontal");
         float verticalThrow = Input.GetAxis("Vertical");
         */
+
         Debug.Log(horizontalThrow + ", " + verticalThrow);
+
+        transform.localPosition = new Vector3
+            (
+            transform.localPosition.x + horizontalThrow,
+            transform.localPosition.y + verticalThrow,
+            transform.localPosition.z
+            );
     }
 }
